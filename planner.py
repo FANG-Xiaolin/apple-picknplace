6d55cfa129b44dc3ffa74259abb608af90b3eb39
xiaolinf <fxlfang@gmail.com>
2025-04-22 13:50:09 -0400

    [main] cleaner interfaces

--- planner.py
+++ planner.py
@@ -30,6 +30,7 @@ class Command(ABC):
         self.name = name
         self.args = args
 
+CommandSequence = list[Command]
 
 class JointPathCommand(Command):
     def __init__(self, action: JointPath):
@@ -51,105 +52,59 @@ class GraspParameter:
     pregrasp_pose: Mat4 # mat 4
     grasp_pose: Mat4 # mat 4
 
-CommandSequence = list[Command]
+#####################################  IK Solver #####################################
+class IKSolver(ABC):
+    def __init__(self, urdf_path: str, tool_link_name: str):
+        self.urdf_path = urdf_path
+        self.tool_link_name = tool_link_name
 
-class MotionPlanner:
-    def __init__(self, config):
-        # Initialize the motion planner
-        self.grasp_sampler = TopDownGraspSampler()
-        self.iksolver = self.create_tracik_solver(config.iksolver_config.urdf_path, config.iksolver_config.tool_link_name)
+    def ik(self, goal: Mat4, qinit: JointConf) -> Optional[JointConf]:
+        # Implement the inverse kinematics solver
+        pass
 
-    def create_tracik_solver(self, urdf_path, tool_link_name, max_time=0.0025, error=1e-2):
+
+class TracIKSolver(IKSolver):
+    def __init__(self, urdf_path: str, tool_link_name: str):
+        super().__init__(urdf_path, tool_link_name)
         from tracikpy import TracIKSolver
         tracik_solver = TracIKSolver(
             urdf_file=urdf_path,
             base_link='panda_link0',
             tip_link=tool_link_name,
-            timeout=max_time,
-            epsilon=error,
+            timeout=0.0025,
+            epsilon=1e-2,
             solve_type="Distance",
         )  # Speed | Distance | Manipulation1 | Manipulation2
         assert tracik_solver.joint_names
         tracik_solver.urdf_file = urdf_path
-        return tracik_solver
+        self.solver = tracik_solver
 
-    def plan_arm_path(self, start_conf: JointConf, goal: Mat4) -> list[JointConf]:
+    def ik(self, goal: Mat4, qinit: JointConf) -> Optional[JointConf]:
+        return self.solver.ik(goal, qinit=qinit)
+############################################################################################
+
+######################################  Motion Planner #####################################
+class MotionPlanner(ABC):
+    def __init__(self, config):
+        self.config = config
+
+    def plan(self, start_conf: JointConf, goal_conf: Mat4, *args) -> JointPath:
         # Implement the motion planning algorithm
-        end_conf = self.iksolver.ik(goal, qinit=start_conf)
-        if end_conf is None:
-            return None
+        # Could use RRT, PRM, etc.
+        pass
+
+class InterpolationMotionPlanner(MotionPlanner):
+    def __init__(self, config):
+        super().__init__(config)
+
+    def plan(self, start_conf: JointConf, goal_conf: Mat4, *args) -> JointPath:
+        # Implement the interpolation motion planning algorithm
         n_step = 100
-        arm_path = [start_conf + (end_conf - start_conf) * i / n_step for i in range(n_step)] + [end_conf]
+        arm_path = [start_conf + (goal_conf - start_conf) * i / n_step for i in range(n_step)] + [goal_conf]
         return arm_path
+############################################################################################
 
-    def sample_grasp_pose(self, pcd_cameraframe, pcd_worldframe, rgb_im, target_object_mask) -> Iterator[GraspParameter]:
-        # Execute the planned motion
-        target_object_pcd_worldframe = pcd_worldframe[target_object_mask]
-        target_object_pcd_worldframe = target_object_pcd_worldframe[(target_object_pcd_worldframe!=0).any(axis=1)]
-        for rv in self.grasp_sampler.sample_grasp(target_object_pcd_worldframe):
-            yield rv
-
-    def generate_grasp_path(self, start_qpos: JointConf, grasp_param: GraspParameter) -> CommandSequence:
-        # Generate the path for the robot to grasp the object
-        path_to_pregrasp = self.plan_arm_path(start_qpos, grasp_param.pregrasp_pose)
-        if path_to_pregrasp is None:
-            return None
-        return CommandSequence([
-            GripperMotionCommand('open'),
-            JointPathCommand(path_to_pregrasp), CartesianGoalCommand(grasp_param.grasp_pose),
-            GripperMotionCommand('close'), CartesianGoalCommand(grasp_param.pregrasp_pose)
-        ])
-
-    def plan_picking(self, start_qpos: JointConf, pcd_cameraframe, pcd_worldframe, rgb_im, target_object_mask) -> CommandSequence:
-        for grasp_param in self.sample_grasp_pose(pcd_cameraframe, pcd_worldframe, rgb_im, target_object_mask):
-            picking_command = self.generate_grasp_path(start_qpos, grasp_param)
-            if picking_command is not None:
-                return picking_command
-
-    def plan_placing(self, qpos: JointConf, x_range, y_range, max_iter=10) -> CommandSequence:
-        for _ in range(max_iter):
-            placement_param = self.sample_placement_pose(x_range, y_range)
-            placing_command = self.generate_placement_path(qpos, placement_param)
-            if placing_command is not None:
-                return placing_command
-
-    def sample_placement_pose(self, x_range, y_range) -> GraspParameter:
-        # Sample a placement pose for the object
-        x_loc = np.random.uniform(x_range[0], x_range[1])
-        y_loc = np.random.uniform(y_range[0], y_range[1])
-        z = 0.05 # Fixed height for placement
-        place_pose = np.eye(4)
-        place_pose[:3, 3] = [x_loc, y_loc, z]
-        preplace_pose = place_pose.copy()
-        preplace_pose[2, 3] = z + 0.05
-        return GraspParameter(world2ee(preplace_pose), world2ee(place_pose))
-
-    def generate_placement_path(self, start_qpos: JointConf, grasp_param: GraspParameter) -> CommandSequence:
-        # Generate the path for the robot to place the object
-        path_to_pregrasp = self.plan_arm_path(start_qpos, grasp_param.pregrasp_pose)
-        if path_to_pregrasp is None:
-            return None
-        return CommandSequence([
-            JointPathCommand(path_to_pregrasp), CartesianGoalCommand(grasp_param.grasp_pose),
-            GripperMotionCommand('open'), CartesianGoalCommand(grasp_param.pregrasp_pose)
-        ])
-
-
-def get_xyz_extend_from_mask(point_cloud, mask):
-    """
-    Get the XYZ extent from the point cloud and mask.
-    Args:
-        point_cloud (np.ndarray): The point cloud data.
-        mask (np.ndarray): The mask for the object.
-    Returns:
-        tuple: The limits of the point cloud.
-    """
-    point_valid = point_cloud[mask]
-    min_x, min_y, min_z = point_valid.min(axis=0)
-    max_x, max_y, max_z = point_valid.max(axis=0)
-    return min_x, max_x, min_y, max_y, min_z, max_z
-
-
+######################################  Grasp Sampler ######################################
 class GraspSampler(ABC):
     def __init__(self):
         # Initialize the grasp sampler
@@ -207,6 +162,21 @@ def world2ee(pose: Mat4) -> Mat4:
     return pose.dot(world2ee_tform)
 
 
+def get_xyz_extend_from_mask(point_cloud, mask):
+    """
+    Get the XYZ extent from the point cloud and mask.
+    Args:
+        point_cloud (np.ndarray): The point cloud data.
+        mask (np.ndarray): The mask for the object.
+    Returns:
+        tuple: The limits of the point cloud.
+    """
+    point_valid = point_cloud[mask]
+    min_x, min_y, min_z = point_valid.min(axis=0)
+    max_x, max_y, max_z = point_valid.max(axis=0)
+    return min_x, max_x, min_y, max_y, min_z, max_z
+
+
 class AnyGraspSampler(GraspSampler):
     def __init__(self, config):
         # Initialize the any grasp sampler
@@ -251,19 +221,82 @@ class AnyGraspSampler(GraspSampler):
             grasp_pose = np.eye(4)
             grasp_pose[:3, :3] = g.rotation_matrix
             grasp_pose[:3, 3] = g.translation
-            yield {
-                'score': g.score,
-                'c2g_translation': g.translation,
-                'c2g_rotation_matrix': g.rotation_matrix,
-                'c2g_mat4': grasp_pose
-            }
+            yield grasp_pose
+############################################################################################
+
+class Planner:
+    def __init__(self, config):
+        # Initialize the motion planner
+        self.grasp_sampler = TopDownGraspSampler()
+        self.iksolver = TracIKSolver(config.iksolver_config.urdf_path, config.iksolver_config.tool_link_name)
+        self.motion_planner = InterpolationMotionPlanner(config)
+
+    def plan_arm_path(self, start_conf: JointConf, goal_conf: Mat4) -> list[JointConf]:
+        # Implement the motion planning algorithm
+        end_conf = self.iksolver.ik(goal_conf, qinit=start_conf)
+        if end_conf is None:
+            return None
+        arm_path = self.motion_planner.plan(start_conf, end_conf)
+        return arm_path
+
+    def sample_grasp_pose(self, pcd_cameraframe, pcd_worldframe, rgb_im, target_object_mask) -> Iterator[GraspParameter]:
+        # Execute the planned motion
+        target_object_pcd_worldframe = pcd_worldframe[target_object_mask]
+        target_object_pcd_worldframe = target_object_pcd_worldframe[(target_object_pcd_worldframe!=0).any(axis=1)]
+        for rv in self.grasp_sampler.sample_grasp(target_object_pcd_worldframe):
+            yield rv
+
+    def generate_grasp_path(self, start_qpos: JointConf, grasp_param: GraspParameter) -> CommandSequence:
+        # Generate the path for the robot to grasp the object
+        path_to_pregrasp = self.plan_arm_path(start_qpos, grasp_param.pregrasp_pose)
+        if path_to_pregrasp is None:
+            return None
+        return CommandSequence([
+            GripperMotionCommand('open'),
+            JointPathCommand(path_to_pregrasp), CartesianGoalCommand(grasp_param.grasp_pose),
+            GripperMotionCommand('close'), CartesianGoalCommand(grasp_param.pregrasp_pose)
+        ])
+
+    def plan_picking(self, start_qpos: JointConf, pcd_cameraframe, pcd_worldframe, rgb_im, target_object_mask) -> CommandSequence:
+        for grasp_param in self.sample_grasp_pose(pcd_cameraframe, pcd_worldframe, rgb_im, target_object_mask):
+            picking_command = self.generate_grasp_path(start_qpos, grasp_param)
+            if picking_command is not None:
+                return picking_command
+
+    def plan_placing(self, qpos: JointConf, x_range, y_range, max_iter=10) -> CommandSequence:
+        for _ in range(max_iter):
+            placement_param = self.sample_placement_pose(x_range, y_range)
+            placing_command = self.generate_placement_path(qpos, placement_param)
+            if placing_command is not None:
+                return placing_command
+
+    def sample_placement_pose(self, x_range, y_range) -> GraspParameter:
+        # Sample a placement pose for the object
+        x_loc = np.random.uniform(x_range[0], x_range[1])
+        y_loc = np.random.uniform(y_range[0], y_range[1])
+        z = 0.05 # Fixed height for placement
+        place_pose = np.eye(4)
+        place_pose[:3, 3] = [x_loc, y_loc, z]
+        preplace_pose = place_pose.copy()
+        preplace_pose[2, 3] = z + 0.05
+        return GraspParameter(world2ee(preplace_pose), world2ee(place_pose))
+
+    def generate_placement_path(self, start_qpos: JointConf, grasp_param: GraspParameter) -> CommandSequence:
+        # Generate the path for the robot to place the object
+        path_to_pregrasp = self.plan_arm_path(start_qpos, grasp_param.pregrasp_pose)
+        if path_to_pregrasp is None:
+            return None
+        return CommandSequence([
+            JointPathCommand(path_to_pregrasp), CartesianGoalCommand(grasp_param.grasp_pose),
+            GripperMotionCommand('open'), CartesianGoalCommand(grasp_param.pregrasp_pose)
+        ])
 
 
 def initialize_planning_interface(config):
     """
     Initialize the planning interface.
     Returns:
-        MotionPlanner: The initialized planning interface.
+        Planner: The initialized planning interface.
     """
-    planning_interface = MotionPlanner(config)
+    planning_interface = Planner(config)
     return planning_interface
